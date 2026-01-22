bfa52c2520725e8a475310f12852e640c35af7f3
Fabio Andre Martins Coelho <fandre-m@c1r8s7.42porto.com>
2026-01-22 17:30:49 +0000

    Sometimes I'm blind as a bat

--- Python_00/ex6/ft_garden_summary.py
+++ Python_00/ex6/ft_garden_summary.py
@@ -1,6 +1,6 @@
 def ft_garden_summary():
-    g_name = str(input("Enter garden name: "))
-    num_plants = int(input("Enter number of plants: "))
+    g_name = input("Enter garden name: ")
+    num_plants = input("Enter number of plants: ")
     print("Garden:", g_name)
     print("Plants:", num_plants)
     print("Status: Growing well!")
