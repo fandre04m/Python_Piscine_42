5d54293ba66f73255b1a948435a17412b12a43df
Fabio Andre Martins Coelho <fandre-m@c1r8s7.42porto.com>
2026-03-03 16:46:18 +0000

    Correct exercise 04/1

--- Python_04/ex1/ft_archive_creation.py
+++ Python_04/ex1/ft_archive_creation.py
@@ -3,30 +3,27 @@
 
 def ft_archive_creation() -> None:
     print("=== CYBER ARCHIVES - PRESERVATION SYSTEM ===\n")
-    file = None
+    archive = None
     try:
-        file = open("new_discovery.txt", "w")
-        print(f"Initializing new storage unit: {file.name}")
+        archive = open("new_discovery.txt", "w")
+        print(f"Initializing new storage unit: {archive.name}")
         print("Storage unit created successfully...\n")
         print("Inscribing preservation data...")
-        file.write(
+        entries = (
             "[ENTRY 001] New quantum algorithm discovered\n"
             "[ENTRY 002] Efficiency increased by 347%\n"
             "[ENTRY 003] Archived by Data Archivist trainee"
         )
-        print(
-            "[ENTRY 001] New quantum algorithm discovered\n"
-            "[ENTRY 002] Efficiency increased by 347%\n"
-            "[ENTRY 003] Archived by Data Archivist trainee"
-        )
+        archive.write(entries)
+        print(entries)
         print("\nData inscription complete. Storage unit sealed.")
-        print(f"Archive '{file.name}' ready for long time preservation.")
-        file.close()
+        print(f"Archive '{archive.name}' ready for long-term preservation.")
+        archive.close()
     except OSError as e:
         print(e)
-        print("Closing archive securelly")
-        if file is not None:
-            file.close()
+        print("Closing archive securely")
+        if archive is not None:
+            archive.close()
 
 
 if __name__ == "__main__":
