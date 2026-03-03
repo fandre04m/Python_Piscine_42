d8e6d5cc868efa4c7ba15f9dd1f5640d3cb77c50
Fabio Andre Martins Coelho <fandre-m@c1r8s7.42porto.com>
2026-03-03 15:07:41 +0000

    Add exercise 04/4

---
+++ Python_04/ex4/ft_crisis_response.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+
+
+def error_handler(file_name: str) -> None:
+    try:
+        with open(file_name) as file:
+            print(f"ROUTINE ACCESS: Attempting access to '{file.name}'...")
+            print(f"SUCCESS: Archive recovered - '{file.read()}'")
+            print("STATUS: Normal operations resumed")
+    except FileNotFoundError:
+        print(f"CRISIS ALERT: Attempting access to '{file_name}'...")
+        print("RESPONSE: Archive not found in storage matrix")
+        print("STATUS: Crisis handled, system stable")
+    except PermissionError:
+        print(f"CRISIS ALERT: Attempting access to '{file_name}'...")
+        print("RESPONSE: Security protocols deny access")
+        print("STATUS: Crisis handled, security mantained")
+    except OSError as e:
+        print(e)
+
+
+def ft_crisis_response() -> None:
+    print("=== CYBER ARCHIVES - CRISIS RESPONSE SYSTEM ===\n")
+    error_handler("lost_archive.txt")
+    print()
+    error_handler("classified_data.txt")
+    print()
+    error_handler("standard_archive.txt")
+    print("\nAll crisis scenarios handled successfully. Archives secure.")
+
+
+if __name__ == "__main__":
+    ft_crisis_response()
