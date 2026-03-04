d8c6cb8ddecaa47d0cc9a1ebcdde885369ec2061
Fabio Andre Martins Coelho <fandre-m@c1r8s7.42porto.com>
2026-03-04 14:41:27 +0000

    Change second open for 'w' mode instead of 'a'

--- Python_04/ex3/ft_vault_security.py
+++ Python_04/ex3/ft_vault_security.py
@@ -5,15 +5,15 @@ def ft_vault_security() -> None:
     print("=== CYBER ARCHIVES - VAULT SECURITY SYSTEM ===\n")
     print("Initiating secure vault access...")
     try:
-        with open("classified_data.txt") as c_file:
+        with open("classified_data.txt", "r") as c_file:
             print("Vault connection established with failsafe protocols\n")
             print("SECURE EXTRACTION:")
             print(c_file.read())
-        with open("classified_data.txt", "a") as s_file:
-            message = "[CLASSIFIED] New security protocols archived"
+        with open("security_protocols.txt", "w") as s_file:
+            message = "[CLASSIFIED] New security protocols archived\n"
             s_file.write(message)
             print("SECURE PRESERVATION:")
-            print(message)
+            print(message, end="")
         print("Vault automatically sealed upon completion")
     except OSError as e:
         print(e)
