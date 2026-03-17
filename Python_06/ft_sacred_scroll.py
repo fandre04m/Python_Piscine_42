5451be2d9fcb12370deaa178be3337a4332469d8
Fabio Andre Martins Coelho <fandre-m@c1r8s7.42porto.com>
2026-03-17 20:49:32 +0000

    First few bits of py06

---
+++ Python_06/ft_sacred_scroll.py
@@ -0,0 +1,7 @@
+from alchemy import create_fire, create_water
+
+
+print("=== Sacred Scroll Mastery ===")
+print()
+print("Testing direct module access:")
+print(f"alchemy.elements.create_fire(): {alchemy.elements.create_fire()}")
