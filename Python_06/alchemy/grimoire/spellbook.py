184243748b29af07b4afd73237b4137329ba0ec2
Fabio Andre Martins Coelho <fandre-m@c1r8s7.42porto.com>
2026-04-08 13:48:08 +0100

    Finally understood relative imports

--- Python_06/alchemy/grimoire/spellbook.py
+++
@@ -1,8 +0,0 @@
-
-
-def record_spell(spell_name: str, ingredients: str) -> str:
-    from .validator import validate_ingredients
-    validation: str = validate_ingredients(ingredients)
-    if "INVALID" in validation:
-        return f"Spell rejected: {spell_name} ({validation})"
-    return f"Spell recorded: {spell_name} ({validation})"
--- Python_06/ft_trasmutation_0.py
+++
@@ -1,9 +0,0 @@
-import alchemy.transmutation.recipes
-
-
-def main() -> None:
-    print(alchemy.transmutation.recipes.lead_to_gold())
-
-
-if __name__ == "__main__":
-    main()
